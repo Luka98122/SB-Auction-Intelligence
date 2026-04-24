52ed0e7361ca23d6abf4774976e571d5b21dc1ab
Luka Markovic <luka98122@gmail.com>
2026-04-24 22:44:22 +0200

    size.sh bugfix

--- helper/size.sh
+++ helper/size.sh
@@ -3,6 +3,9 @@
 # Load variables from .env
 export $(grep -v '^#' .env | xargs)
 
+# Force MySQL to update statistics for the auctions table
+docker exec -i skyblock_mysql mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE -e "ANALYZE TABLE auctions;" > /dev/null
+
 echo "--- SB-Auction-Intelligence: Storage Report ---"
 
 docker exec -it skyblock_mysql mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE -e "
